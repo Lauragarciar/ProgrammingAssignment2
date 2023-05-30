34b3d1bcd1938ebfdf41419368365f2de2bcb51a
Lauragarciar <130985038+Lauragarciar@users.noreply.github.com>
2023-05-30 13:07:58 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We need to make two functions
 
-## Write a short comment describing this function
+#makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+
+## This function makeCacheMatrix gets a matrix as an input
+#set the value of the matrix,
+#get the value of the matrix, set the inverse Matrix
+#and get the inverse Matrix. 
+
+#<<- operator is used to assign a value to an object in an
+#environment that is different from the current environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverseMatrix <- NULL
+  setMatrix <- function(y) {
+    x <<- y
+    inverseMatrix <<- NULL
+  }
+  getMatrix <- function() x
+  setinverse <- function(inverse) inverseMatrix <<- inverse
+  getinverse <- function() inverseMatrix
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+#cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+#If the value is not previously calculated the goes to the original makeCacheMatrix and does teh calculation
+
+###We get the value of the Inverse Matrix from the makeCacheMatrix
+## If return gives a NULL value calculates again inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverseMatrix <- x$getinverse()
+  if(!is.null(inverseMatrix)) {
+    message("getting cached data")
+    return(inverseMatrix)
+  }
+  Matrixdata <- x$getMatrix()
+  inverseMatrix <- solve(Matrixdata, ...)
+  x$setInverse(inverseMatrix)
+  return(inverseMatrix)
 }
